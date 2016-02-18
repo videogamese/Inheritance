b998f490fb81e4b9f1f053a4b24c4c5c516476c9
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2016-02-18 08:42:08 -0800

    1.2b
    Removed parameter from attack method

--- Inheritance/WeaponInterface.java
+++ Inheritance/WeaponInterface.java
@@ -10,10 +10,10 @@ public interface WeaponInterface
     /**
      * Attacks an enemy for an amount
      * 
-     * @param  amount Amount of damage to the opponent
+     * 
      * @return        Returns amount of damage successfully done to the opponent 
      */
-    int attack(int amount);
+    int attack();
     /**
      * Parry Attack Method
      * 
